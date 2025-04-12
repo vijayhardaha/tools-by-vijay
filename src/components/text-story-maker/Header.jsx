1e2ca449b4cfa67fd3f610ba9d17ef1cd9ae6a29
Vijay Hardaha <vijayhardaha@gmail.com>
2025-04-12 20:08:58 +0530

    refactor: enhance download functionality in Header component; add loading state and error handling

--- src/components/text-story-maker/Header.jsx
+++ src/components/text-story-maker/Header.jsx
@@ -7,6 +7,7 @@ import { PiResize as SizeIcon } from "react-icons/pi";
 import { PiPaintBucket as BgFillIcon } from "react-icons/pi";
 import { PiCheckCircleFill as CheckFillIcon } from "react-icons/pi";
 import { PiDownloadSimple as DownloadIcon } from "react-icons/pi";
+import { useState } from "react";
 
 import { cardRatios } from "@/components/text-story-maker/constants/cardRatios";
 import Dropdown, {
@@ -89,6 +90,9 @@ const Header = ({
   toolbarVisible,
   setToolbarVisible,
 }) => {
+  const [isDownloading, setIsDownloading] = useState(false);
+  const [downloadError, setDownloadError] = useState("");
+
   const handleSizeSelect = (size) => {
     updateOption("cardRatio", size);
   };
@@ -114,17 +118,27 @@ const Header = ({
       },
     };
 
+    setIsDownloading(true);
+    setDownloadError("");
+
     const downloadFn =
       format === "jpeg"
         ? domToImage.toJpeg(node, options)
         : domToImage.toPng(node, options);
 
-    downloadFn.then((dataUrl) => {
-      const link = document.createElement("a");
-      link.href = dataUrl;
-      link.download = `text-story.${format}`;
-      link.click();
-    });
+    downloadFn
+      .then((dataUrl) => {
+        const link = document.createElement("a");
+        link.href = dataUrl;
+        link.download = `text-story.${format}`;
+        link.click();
+      })
+      .catch(() => {
+        setDownloadError("Download failed");
+      })
+      .finally(() => {
+        setIsDownloading(false);
+      });
   };
 
   return (
@@ -205,58 +219,77 @@ const Header = ({
                   <Button icon={DownloadIcon} srText="Download Image" />
                 </DropdownTrigger>
                 <DropdownContent isOpen={isOpen}>
-                  <div className="mb-2">
-                    <p className="mb-1 text-xs font-semibold text-neutral-700">
-                      Size:
-                    </p>
-                    <div className="flex gap-1">
-                      {["1x", "2x", "3x"].map((size) => (
-                        <button
-                          key={size}
-                          type="button"
-                          className={cn(
-                            "relatve flex shrink-0 items-center justify-center",
-                            "rounded-md px-3 py-1 text-xs font-medium",
-                            "cursor-pointer outline-none focus-visible:outline-none",
-                            "transition-transform duration-300 ease-in-out active:scale-97",
-                            options.downloadSize === size
-                              ? "bg-neutral-900 text-neutral-100"
-                              : "bg-neutral-200 text-neutral-900 hover:bg-neutral-300"
-                          )}
-                          onClick={() => handleDownloadSizeSelect(size)}
-                        >
-                          {size}
-                        </button>
-                      ))}
+                  <div
+                    className={cn({
+                      "pointer-events-none opacity-50": isDownloading,
+                    })}
+                  >
+                    <div className="mb-2">
+                      <p className="mb-1 text-xs font-semibold text-neutral-700">
+                        Size:
+                      </p>
+                      <div className="flex gap-1">
+                        {["1x", "2x", "3x"].map((size) => (
+                          <button
+                            key={size}
+                            type="button"
+                            className={cn(
+                              "relatve flex shrink-0 items-center justify-center",
+                              "rounded-md px-3 py-1 text-xs font-medium",
+                              "cursor-pointer outline-none focus-visible:outline-none",
+                              "transition-transform duration-300 ease-in-out active:scale-97",
+                              options.downloadSize === size
+                                ? "bg-neutral-900 text-neutral-100"
+                                : "bg-neutral-200 text-neutral-900 hover:bg-neutral-300"
+                            )}
+                            onClick={() => handleDownloadSizeSelect(size)}
+                            disabled={isDownloading}
+                          >
+                            {size}
+                          </button>
+                        ))}
+                      </div>
                     </div>
-                  </div>
-                  <div>
-                    <p className="mb-1 text-xs font-semibold text-neutral-700">
-                      Download:
-                    </p>
-                    <div className="grid grid-cols-2 gap-2">
-                      {["JPEG", "PNG"].map((format) => (
-                        <button
-                          key={format}
-                          type="button"
-                          className={cn(
-                            "relatve flex shrink-0 items-center justify-center",
-                            "rounded-md px-3 py-1 text-xs font-semibold",
-                            "cursor-pointer outline-none focus-visible:outline-none",
-                            "transition-transform duration-300 ease-in-out active:scale-97",
-                            "hover:bg-accent-foreground bg-neutral-900 text-neutral-100 hover:text-neutral-900"
-                          )}
-                          onClick={() =>
-                            handleDownload(
-                              format.toLowerCase(),
-                              options.downloadSize
-                            )
-                          }
-                        >
-                          {format}
-                        </button>
-                      ))}
+                    <div>
+                      <p className="mb-1 text-xs font-semibold text-neutral-700">
+                        Download:
+                      </p>
+                      <div className="grid grid-cols-2 gap-2">
+                        {["JPEG", "PNG"].map((format) => (
+                          <button
+                            key={format}
+                            type="button"
+                            className={cn(
+                              "relatve flex shrink-0 items-center justify-center",
+                              "rounded-md px-3 py-1 text-xs font-semibold",
+                              "cursor-pointer outline-none focus-visible:outline-none",
+                              "transition-transform duration-300 ease-in-out active:scale-97",
+                              "hover:bg-accent-foreground bg-neutral-900 text-neutral-100 hover:text-neutral-900"
+                            )}
+                            onClick={() =>
+                              handleDownload(
+                                format.toLowerCase(),
+                                options.downloadSize
+                              )
+                            }
+                            disabled={isDownloading}
+                          >
+                            {format}
+                          </button>
+                        ))}
+                      </div>
                     </div>
+                    {downloadError && (
+                      <p className="mt-2 text-xs font-medium text-red-500">
+                        {downloadError}
+                      </p>
+                    )}
+
+                    {isDownloading && (
+                      <p className="mt-2 text-xs font-medium text-neutral-600">
+                        Downloading...
+                      </p>
+                    )}
                   </div>
                 </DropdownContent>
               </>
