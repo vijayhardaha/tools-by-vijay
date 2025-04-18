d7047177747cd2de59defab73a7c0490dfcec3a8
Vijay Hardaha <vijayhardaha@gmail.com>
2025-04-19 03:55:49 +0530

    chore: update dependencies and add TypeScript configuration
    - Updated @putout/minify to version 5.0.1
    - Updated next to version 15.3.1
    - Updated react and react-dom to version 19.1.0
    - Updated sanitize-html to version 2.16.0
    - Updated tailwindcss to version 4.1.4
    - Updated various devDependencies including eslint, eslint-config-next, and prettier
    - Added TypeScript configuration file (tsconfig.json) with appropriate settings

---
+++ eslint.config.mjs
@@ -0,0 +1,118 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+import { fixupPluginRules } from "@eslint/compat";
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import { defineConfig, globalIgnores } from "eslint/config";
+import importPlugin from "eslint-plugin-import";
+import jsxA11Y from "eslint-plugin-jsx-a11y";
+import prettier from "eslint-plugin-prettier";
+import react from "eslint-plugin-react";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default defineConfig([
+  globalIgnores([
+    "**/node_modules/",
+    "**/build/",
+    "**/dist/",
+    "**/coverage/",
+    "**/.env",
+    "**/.env.local",
+    "**/*.log",
+    "**/bower_components/",
+    "**/*.tsbuildinfo",
+    "test/**/*.js",
+    "spec/**/*.js",
+    "**/*.generated.js",
+  ]),
+  {
+    extends: compat.extends(
+      "next",
+      "next/core-web-vitals",
+      "eslint:recommended",
+      "plugin:react/recommended",
+      "plugin:jsx-a11y/recommended",
+      "plugin:prettier/recommended",
+      "plugin:@typescript-eslint/recommended"
+    ),
+
+    plugins: {
+      react,
+      typescript: tseslint.plugin,
+      "jsx-a11y": jsxA11Y,
+      importPlugin: fixupPluginRules(importPlugin),
+      prettier,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+
+      parser: tseslint.parser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        project: "./tsconfig.json",
+      },
+    },
+
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "prettier/prettier": ["warn", {}, { usePrettierrc: true }],
+
+      "react/no-unknown-property": [
+        "error",
+        {
+          ignore: ["jsx", "global"],
+        },
+      ],
+
+      "importPlugin/order": [
+        "error",
+        {
+          groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
+
+          pathGroups: [
+            {
+              pattern: "react",
+              group: "external",
+              position: "before",
+            },
+          ],
+
+          pathGroupsExcludedImportTypes: ["react"],
+
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+
+          "newlines-between": "always",
+          warnOnUnassignedImports: true,
+        },
+      ],
+    },
+  },
+]);
