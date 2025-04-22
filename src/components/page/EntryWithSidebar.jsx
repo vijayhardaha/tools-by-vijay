f809488ee773fe5245b3a198eb1d3545326c12f7
Vijay Hardaha <vijayhardaha@gmail.com>
2025-04-23 02:42:06 +0530

    feat: add EntryWithSidebar component for layout with sidebar functionality

---
+++ src/components/page/EntryWithSidebar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import PropTypes from "prop-types";
+
+import ToolsListWidget from "@/components/page/ToolsListWidget";
+
+/**
+ * A layout component that displays a page entry with a sidebar.
+ *
+ * @param {Object} props - The component props.
+ * @param {Object} props.tool - The tool object containing name and category.
+ * @param {React.ReactNode} props.children - The main content to display in the entry area.
+ * @returns {JSX.Element} The rendered component.
+ */
+const EntryWithSidebar = ({ tool, children }) => {
+  return (
+    <div className="grid grid-cols-6 gap-4">
+      <main className="col-span-4">{children}</main>
+
+      <aside className="col-span-2">
+        <ToolsListWidget category={tool.category} hideTool={tool.slug}></ToolsListWidget>
+      </aside>
+    </div>
+  );
+};
+
+EntryWithSidebar.propTypes = {
+  tool: PropTypes.object.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+export default EntryWithSidebar;
