32d26814268bf97ea48e0d4d578a2a2e658d7823
Vijay Hardaha <vijayhardaha@gmail.com>
2025-05-21 15:34:00 +0530

    fix: update sitemapBaseFileName and enhance robotsTxt transformation

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,13 +2,18 @@
 const config = {
   siteUrl: "https://toolsbyvijay.vercel.app",
   generateRobotsTxt: true,
-  sitemapBaseFileName: "next-sitemap",
+  sitemapBaseFileName: "sitemap",
   changefreq: "weekly",
   priority: 0.7,
   trailingSlash: false,
   exclude: ["/404", "/500"],
   robotsTxtOptions: {
     policies: [{ userAgent: "*", allow: "/" }],
+    transformRobotsTxt: async (_, robotsTxt) => {
+      const withoutHost = robotsTxt.replace(`# Host\nHost: https://toolsbyvijay.vercel.app\n\n`, "");
+
+      return withoutHost;
+    },
   },
 };
 
