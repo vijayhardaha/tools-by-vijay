372de0ea8dc76d9ec60b51ad56015ae221209799
Vijay Hardaha <vijayhardaha@proton.me>
2026-06-10 16:25:27 +0530

    refactor: enhance JSON Sorter page
    - Improve page structure and styling for better usability

--- src/app/tools/json-sorter/page.tsx
+++ src/app/tools/json-sorter/page.tsx
@@ -1,3 +1,5 @@
+import type { JSX } from 'react';
+
 import { notFound } from 'next/navigation';
 
 import EntryWithSidebar from '@/components/page/EntryWithSidebar';
@@ -32,9 +34,9 @@ export const metadata: PageMeta = generateMeta({
  * JSON Sorter tool page component.
  * Renders the page layout with header and the JSON Sorter tool.
  *
- * @returns {React.JSX.Element} The rendered JSON Sorter tool page component.
+ * @returns {JSX.Element} The rendered JSON Sorter tool page component.
  */
-const JsonSorter = (): React.JSX.Element => {
+const JsonSorter = (): JSX.Element => {
   if (!tool) {
     notFound(); // Render a 404 page if the tool is null
   }
