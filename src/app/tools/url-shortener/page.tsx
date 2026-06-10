f2b08651d569e1b773f9e3a1bb0bebcca4cec0b0
Vijay Hardaha <vijayhardaha@proton.me>
2026-06-10 15:55:06 +0530

    refactor: enhance URL Shortener page
    - Improve page structure and styling for better user experience

--- src/app/tools/url-shortener/page.tsx
+++ src/app/tools/url-shortener/page.tsx
@@ -34,7 +34,7 @@ export const metadata: PageMeta = generateMeta({
  *
  * @returns {React.JSX.Element} The rendered URL Shortener tool page component.
  */
-const UrlShortener: React.FC = (): React.JSX.Element => {
+const UrlShortener = (): React.JSX.Element => {
   if (!tool) {
     notFound(); // Render a 404 page if the tool is null
   }
