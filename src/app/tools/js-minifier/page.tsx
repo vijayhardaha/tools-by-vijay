46fefede817234232c9fda96196d14dda1d5ca4f
Vijay Hardaha <vijayhardaha@proton.me>
2026-06-10 15:54:59 +0530

    refactor: enhance JS Minifier page
    - Improve page structure and styling for better usability

--- src/app/tools/js-minifier/page.tsx
+++ src/app/tools/js-minifier/page.tsx
@@ -34,7 +34,7 @@ export const metadata: PageMeta = generateMeta({
  *
  * @returns {React.JSX.Element} The rendered JavaScript Minifier tool page component.
  */
-const JsMinifier: React.FC = (): React.JSX.Element => {
+const JsMinifier = (): React.JSX.Element => {
   if (!tool) {
     notFound(); // Render a 404 page if the tool is null
   }
