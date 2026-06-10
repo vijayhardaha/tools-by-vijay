018505e076537aa782953c19f17bda9497db2021
Vijay Hardaha <vijayhardaha@proton.me>
2026-06-10 15:54:54 +0530

    refactor: enhance Barcode Generator page
    - Improve page structure and styling for better user experience

--- src/app/tools/barcode-generator/page.tsx
+++ src/app/tools/barcode-generator/page.tsx
@@ -34,7 +34,7 @@ export const metadata: PageMeta = generateMeta({
  *
  * @returns {React.JSX.Element} The rendered Barcode Generator tool page component.
  */
-const BarcodeGenerator: React.FC = (): React.JSX.Element => {
+const BarcodeGenerator = (): React.JSX.Element => {
   if (!tool) {
     notFound(); // Render a 404 page if the tool is null
   }
