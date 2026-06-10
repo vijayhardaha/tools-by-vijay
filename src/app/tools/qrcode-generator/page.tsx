7adbacb16b716f25148c1f06ec924af9209347d0
Vijay Hardaha <vijayhardaha@proton.me>
2026-06-10 16:25:29 +0530

    refactor: enhance QR Code Generator page
    - Improve page structure and styling for better user experience

--- src/app/tools/qrcode-generator/page.tsx
+++ src/app/tools/qrcode-generator/page.tsx
@@ -1,3 +1,5 @@
+import type { JSX } from 'react';
+
 import { notFound } from 'next/navigation';
 
 import EntryWithSidebar from '@/components/page/EntryWithSidebar';
@@ -32,9 +34,9 @@ export const metadata: PageMeta = generateMeta({
  * QR Code Generator tool page component.
  * Renders the page layout with header and the QR Code Generator tool.
  *
- * @returns {React.JSX.Element} The rendered QR Code Generator tool page component.
+ * @returns {JSX.Element} The rendered QR Code Generator tool page component.
  */
-const QRCodeGenerator = (): React.JSX.Element => {
+const QRCodeGenerator = (): JSX.Element => {
   if (!tool) {
     notFound(); // Render a 404 page if the tool is null
   }
